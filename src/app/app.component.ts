cd90518f24e49e8d6c44758157e5b0d95395af76
Jeffrey Baldwin <jbaldwin01@snet.net>
2019-03-09 12:11:38 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'pm-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title: string = 'Angular: Getting Started';
+// }
+
+@Component({
+  selector: 'pm-root', // pm for product management, root app component
+  template: `
+  <div>
+    <h1>
+      {{pageTitle}}
+    </h1>
+    <pm-products></pm-products>
+  </div>
+  `
+})
+export class AppComponent {
+  pageTitle: string = 'Acme Product Management';
+}
