bedfb9b6cf9183f3fe4eb6e4fecf430b0726fd53
Jeffrey Baldwin <jbaldwin01@snet.net>
2019-03-10 17:01:01 -0400

    Adjust indentation

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -13,14 +13,14 @@ export class ProductListComponent {
   listFilter: string = '';
   products: IProduct[] = [
     {
-    "productId": 2,
-    "productName": "Garden Cart",
-    "productCode": "GDN-0023",
-    "releaseDate": "March 18, 2016",
-    "description": "15 gallon capacity rolling garden cart",
-    "price": 32.99,
-    "starRating": 4.2,
-    "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2016",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
     },
     {
       "productId": 5,
