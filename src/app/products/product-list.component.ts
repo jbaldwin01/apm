5367bdc03196be49c9634a0cda8fc1c3dfcd4672
Jeffrey Baldwin <jbaldwin01@snet.net>
2019-03-10 15:54:38 -0400

    Implement event binding to toggle image display on button click, use property binding for image properties

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,6 +6,9 @@ import { Component } from "@angular/core";
 })
 export class ProductListComponent {
   pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
   products: any[] = [
     {
     "productId": 2,
@@ -28,4 +31,7 @@ export class ProductListComponent {
       "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
     }
   ];
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 }
