f8d3e6e6866ea96498954ce2918500dc75b1cf03
Jeffrey Baldwin <jbaldwin01@snet.net>
2019-03-13 21:03:38 -0400

    Retrieve product data using HTTP get

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
-import { ProductService } from './products/product.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   declarations: [
     AppComponent,
@@ -18,7 +19,6 @@ import { ProductService } from './products/product.service';
     ConvertToSpacesPipe,
     StarComponent
   ],
-  bootstrap: [AppComponent],
-  providers: [ProductService]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -23,6 +23,7 @@ export class ProductListComponent implements OnInit {
 
   filteredProducts: IProduct[];
   products: IProduct[] = [];
+  errorMessage: string;
 
   constructor(private productService: ProductService) {
   }
@@ -32,8 +33,13 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
-    this.filteredProducts = this.products;
+    this.productService.getProducts().subscribe(
+      products => {
+        this.products = products,
+        this.filteredProducts = this.products;
+      },
+      error => this.errorMessage = <any>error
+    );
   }
 
   performFilter(filterBy: string): IProduct[] {
