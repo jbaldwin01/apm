e0983558a600fea723823ac909668fc71c6f2be5
Jeffrey Baldwin <jbaldwin01@snet.net>
2019-03-16 19:38:44 -0400

    Move inline template to component template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,9 @@
 import { Component } from '@angular/core';
 
-// @Component({
-//   selector: 'pm-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   title: string = 'Angular: Getting Started';
-// }
-
 @Component({
   selector: 'pm-root', // pm for product management, root app component
-  template: `
-  <nav class='navbar navbar-expand navbar-light bg-light'>
-    <a class='navbar-brand'>{{pageTitle}}</a>
-    <ul class='nav nav-pills'>
-      <li><a class='nav-link' [routerLink]="['/welcome']">Home</a></li>
-      <li><a class='nav-link' [routerLink]="['/products']">Product List</a></li>
-    </ul>
-  </nav>
-  <div class='container'>
-      <router-outlet></router-outlet>
-  </div>
-  `
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';
