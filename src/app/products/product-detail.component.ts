bdc6de4e20bbf100cb44a165be93c2408ce95c1c
Jeffrey Baldwin <jbaldwin01@snet.net>
2019-03-16 20:08:02 -0400

    Implement product detail routing, retrieve product data from product service

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -8,10 +10,35 @@ import { IProduct } from './product';
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
+  products: IProduct[] = [];
+  errorMessage: string;
+  imageWidth: number = 200;
+  imageMargin: number = 2;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private productService: ProductService) {
   }
 
+  ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id'); // + converts string to numeric
+
+    this.productService.getProducts().subscribe(
+      products => {
+        this.products = products.filter((product: IProduct) =>
+          product.productId === id),
+        this.product = this.products[0],
+        this.pageTitle += `: ${this.product.productName}`;
+      },
+      error => this.errorMessage = <any>error
+    );
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
+
+  onRatingClicked(message: string): void {
+    this.pageTitle += `: ${message}`;
+  }
 }
